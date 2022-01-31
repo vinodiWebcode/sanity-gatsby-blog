0cc9bb0ce4787b7d3feb94b7e26b2fa9d5d09550
vinodiWebcode <vinod.kumar@iwebcode.dev>
2022-01-31 13:44:56 +0530

    bug fixed

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -61,18 +61,22 @@ const Layout = ({
                 <li className="ftco-animate fadeInUp ftco-animated">
                   <Link to="#">
                     <span className="icon-twitter">
-                    <FaTwitterSquare/>
+                      <FaTwitterSquare />
                     </span>
                   </Link>
                 </li>
                 <li className="ftco-animate fadeInUp ftco-animated">
                   <Link to="#">
-                    <span className="icon-facebook"><GrFacebook/></span>
+                    <span className="icon-facebook">
+                      <GrFacebook />
+                    </span>
                   </Link>
                 </li>
                 <li className="ftco-animate fadeInUp ftco-animated">
                   <Link to="#">
-                    <span className="icon-instagram"><GrInstagram/></span>
+                    <span className="icon-instagram">
+                      <GrInstagram />
+                    </span>
                   </Link>
                 </li>
               </ul>
@@ -162,20 +166,26 @@ const Layout = ({
               <div className="block-23 mb-3">
                 <ul>
                   <li>
-                    <span className="icon icon-map-marker"><BiMap /></span>
+                    <span className="icon icon-map-marker">
+                      <BiMap />
+                    </span>
                     <span className="text">
                       203 Fake St. Mountain View, San Francisco, California, USA
                     </span>
                   </li>
                   <li>
                     <Link to="#">
-                      <span className="icon icon-phone"><VscCallIncoming /></span>
+                      <span className="icon icon-phone">
+                        <VscCallIncoming />
+                      </span>
                       <span className="text">+2 392 3929 210</span>
                     </Link>
                   </li>
                   <li>
                     <Link to="#">
-                      <span className="icon icon-envelope"><AiOutlineMail/></span>
+                      <span className="icon icon-envelope">
+                        <AiOutlineMail />
+                      </span>
                       <span className="text">info@yourdomain.com</span>
                     </Link>
                   </li>
